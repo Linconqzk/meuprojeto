20bf51dfc40a7585876e577939834b18d4bdd101
Lincon <joselinconoliveiradasilvacardo@gmail.com>
2026-09-17 16:51:58 -0300

    adicionar tag

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+console.log("Hello, world")
+
+const lincon = "José Lincon Oliveira Da Silva Cardoso"
+
+console.log(lincon)
+
+let lincon2 = "Olá"
+
+console.log(lincon2)
+
+alexandre2 = "Arroz"
+
+console.log(lincon2)
+
+Document.body.style.backgroundColor = "pink"
